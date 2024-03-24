7aa40a94e70f831506e0526ede1ed01613581c71
tejasmorajkar <tmorajkar316@gmail.com>
2024-03-24 22:57:42 +0530

    updated problem description for longest substring k distinct

--- src/main/java/SlidingWindow/LongestSubstringKDistinct.java
+++ src/main/java/SlidingWindow/LongestSubstringKDistinct.java
@@ -1,3 +1,10 @@
+/**
+ * Given a string, find the length of the longest substring in it with no more than K distinct characters.
+ * Input: String="araaci", K=2
+ * Output: 4
+ * Explanation: The longest substring with no more than '2' distinct characters is "araa".
+ */
+
 package SlidingWindow;
 
 import java.util.HashMap;
