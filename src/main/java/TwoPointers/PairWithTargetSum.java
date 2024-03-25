0366055f8b9b7e8c3772b1eb699720c057d63764
tejasmorajkar <tmorajkar316@gmail.com>
2024-03-25 23:48:10 +0530

    solve 2 easy problems with two pointer technique

---
+++ src/main/java/TwoPointers/PairWithTargetSum.java
@@ -0,0 +1,33 @@
+/**
+ * Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+ * Input: [1, 2, 3, 4, 6], target=6
+ * Output: [1, 3]
+ * Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
+ */
+package TwoPointers;
+
+import java.util.Arrays;
+
+public class PairWithTargetSum {
+    private static int[] search(int[] arr, int targetSum) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int currSum = arr[left] + arr[right];
+            if (currSum < targetSum) {
+                left++;
+            } else if (currSum > targetSum) {
+                right--;
+            } else {
+                return new int[]{left, right};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] result = search(new int[]{1, 2, 3, 4, 6}, 6);
+        System.out.println(Arrays.toString(result));
+        result = search(new int[]{2, 5, 9, 11}, 11);
+        System.out.println(Arrays.toString(result));
+    }
+}
---
+++ src/main/java/TwoPointers/RemoveDuplicates.java
@@ -0,0 +1,55 @@
+package TwoPointers;
+
+public class RemoveDuplicates {
+    /**
+     * Given an array of sorted numbers, remove all duplicates from it.
+     * You should not use any extra space; after removing the duplicates in-place return the new length of the array.
+     * Input: [2, 3, 3, 3, 6, 9, 9]
+     * Output: 4
+     * Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+     */
+    private static int remove(int[] arr) {
+        int nonDuplicateCount = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[nonDuplicateCount - 1] != arr[i]) {
+                arr[nonDuplicateCount] = arr[i];
+                nonDuplicateCount++;
+            }
+        }
+        return nonDuplicateCount;
+    }
+
+    /**
+     * Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and
+     * return the new length of the array.
+     * Input: [3, 2, 3, 6, 3, 10, 9, 3], Key=3
+     * Output: 4
+     * Explanation: The first four elements after removing every 'Key' will be [2, 6, 10, 9].
+     */
+    private static int removeKey(int[] arr, int key) {
+        int nextElement = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != key) {
+                arr[nextElement] = arr[i];
+                nextElement++;
+            }
+        }
+        return nextElement;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 3, 3, 3, 6, 9, 9};
+        int result = remove(nums);
+        System.out.println(result);
+        nums = new int[] {2, 2, 2, 11};
+        result = remove(nums);
+        System.out.println(result);
+
+        nums = new int[]{3, 2, 3, 6, 3, 10, 9, 3};
+        result = removeKey(nums, 3);
+        System.out.println(result);
+        nums = new int[]{2, 11, 2, 2, 1};
+        result = removeKey(nums, 2);
+        System.out.println(result);
+    }
+}
