a537601eb1ed04cc8ca5875c10c5dc26ba04914d
tejasmorajkar <tmorajkar316@gmail.com>
2024-03-26 23:57:01 +0530

    solve more problems related to two pointer technique

---
+++ src/main/java/TwoPointers/DutchNationalFlag.java
@@ -0,0 +1,41 @@
+/**
+ * Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+ * We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+ * You must solve this problem without using the library's sort function.
+ *
+ * Input: nums = [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ */
+package TwoPointers;
+
+import java.util.Arrays;
+
+public class DutchNationalFlag {
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static void sort(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, mid, low);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 0, 2, 1, 0};
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
---
+++ src/main/java/TwoPointers/SubarraysWithProductLessThanTarget.java
@@ -0,0 +1,43 @@
+/**
+ * Given an array with positive numbers and a target number,
+ * find all of its contiguous subarrays whose product is less than the target number.
+ * Input: [2, 5, 3, 10], target=30
+ * Output: [2], [5], [2, 5], [3], [5, 3], [10]
+ * Explanation: There are six contiguous subarrays whose product is less than the target.
+ */
+
+package TwoPointers;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SubarraysWithProductLessThanTarget {
+    private static List<List<Integer>> findSubarrays(int[] arr, int target) {
+        List<List<Integer>> subarrays = new ArrayList<>();
+        int product = 1, left = 0;
+        for (int right = 0; right < arr.length; right++) {
+            product *= arr[right];
+            while (product >= target && left <= right) {
+                product /= arr[left++];
+            }
+            List<Integer> subList = new LinkedList<>();
+            for (int idx = right; idx >= left; idx--) {
+                subList.add(0, arr[idx]);
+                subarrays.add(subList);
+            }
+        }
+        return subarrays;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 5, 3, 10};
+        int target = 30;
+        List<List<Integer>> result = findSubarrays(arr, target);
+        System.out.println(result);
+        int[] arr2 = new int[]{8, 2, 6, 5};
+        int target2 = 50;
+        List<List<Integer>> result2 = findSubarrays(arr2, target2);
+        System.out.println(result2);
+    }
+}
